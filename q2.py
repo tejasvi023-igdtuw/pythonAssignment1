72023f2d165f16c11a7ff0838dc9ff7e43cac9ac
tejasvi023-igdtuw <144039174+tejasvi023-igdtuw@users.noreply.github.com>
2024-06-17 13:14:00 +0530

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,7 @@
+#2. Write a python program that checks whether a given number is even or odd.
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
