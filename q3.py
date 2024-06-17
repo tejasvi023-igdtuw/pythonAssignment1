9dec4e308fc7870615709134a35751861e0a190f
tejasvi023-igdtuw <144039174+tejasvi023-igdtuw@users.noreply.github.com>
2024-06-17 13:22:54 +0530

    Add files via upload

---
+++ q3.py
@@ -0,0 +1,10 @@
+#3. Write a python program that calculates the factorial of a given number.
+
+num = int(input("Enter a number: "))
+factorial = 1
+
+for i in range(1, num + 1):
+    factorial *= i
+
+print(f"The factorial of {num} is {factorial}")
+
---
+++ q4.py
@@ -0,0 +1,6 @@
+#4. Write a program that asks the user for their name and then prints a greeting msg.
+# Ask the user for their name
+name = input("Enter your name: ")
+
+# Print a greeting message
+print(f"Hello, {name}! Nice to meet you.")
